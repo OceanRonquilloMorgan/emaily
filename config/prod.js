b8d0523d3aaab2d7520b2815dfd631ef76a4741d
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-05-22 23:20:34 -1000

    anotha error fixed

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // prod.js - production keys here
 module.exports = {
-  googleClientID: 'process.env.GOOGLE_CLIENT_ID',
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SERET,
   mongoURI: process.env.MONGO_URI,
   // made this up; something someone can't guess
