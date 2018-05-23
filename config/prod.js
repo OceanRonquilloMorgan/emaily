252d5915c5eb51a2230581fc8db2df11049b6abe
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-05-22 23:27:34 -1000

    hate myself

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js - production keys here
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SERET,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   // made this up; something someone can't guess
   cookieKey: process.env.COOKIE_KEY
