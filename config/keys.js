973ea0ddd302045f41977fd0252922cb0dad4b77
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-05-22 23:26:58 -1000

    rlly

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   // we are in development - return the dev keys
-  model.exports = require('./dev');
+  module.exports = require('./dev');
 }
