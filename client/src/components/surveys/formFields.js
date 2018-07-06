8d8329cbeb7747387b8f81409aba208c6504ef3d
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-07-07 01:52:23 +0200

    Minor change to Campaign Title

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -1,5 +1,5 @@
 export default [
-  { label: 'Survey Title', name: 'title' },
+  { label: 'Campaign Title', name: 'title' },
   { label: 'Subject Line', name: 'subject' },
   { label: 'Email Body', name: 'body' },
   { label: 'Recipient List', name: 'recipients' }
