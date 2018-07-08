4202cf0dcff1a82455028669a831b4ea7fe6952c
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-07-08 14:05:10 +0200

    Improved landing user experience

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -13,22 +13,75 @@ const Landing = () => {
             style={{ marginTop: '75px' }}
           />
         </a>
-      </div>
-      <div class="row center">
-        <h5 class="header col s12" style={{ marginTop: '30px' }}>
-          Simple survey collection service to collect feedback from your users
-        </h5>
+        <div class="row center">
+          <h5 class="header col s12" style={{ marginTop: '20px' }}>
+            Simple survey collection service to collect feedback from your users
+          </h5>
+        </div>
+
+        <div class="row center" style={{ marginTop: '10px' }}>
+          <a
+            href="/auth/google"
+            id="download-button"
+            class="waves-effect waves-light btn-large red lighten-3"
+          >
+            Get Started
+          </a>
+        </div>
       </div>
 
-      <div class="row center" style={{ marginTop: '10px' }}>
-        <a
-          href="/auth/google"
-          id="download-button"
-          class="btn-large waves-effect waves-light red lighten-3"
-        >
-          Get Started
-        </a>
-      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <footer class="page-footer red lighten-3">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="center">
+                <i class="center small material-icons">drafts</i>
+              </h5>
+              <h5 class="center white-text">What is Emaily?</h5>
+              <p class="grey-text text-lighten-4">
+                Emaily is a simple, hassle-free service that allows you to send
+                mass emails to a large list of users for the purpose of
+                collecting feedback. With this platform, you can simplify the
+                way you drive customer engagement and achieve your business
+                goals with email marketing campaigns that allow you to track
+                service results.
+              </p>
+            </div>
+            <div class="col l6 s12">
+              <h5 class="center">
+                <i class="center small material-icons">smartphone</i>
+              </h5>
+              <h5 class="center white-text">How to Use Emaily</h5>
+              <p class="grey-text text-lighten-4">
+                To use our service, simply sign-up with any Google account. Add
+                survey credits with a click of the button, curate a new survey
+                form, and begin sending out email campaigns to designated users.
+                With convenience and a seamless user experience in mind, you can
+                watch as your customers respond to your inquiry while we take
+                care of the rest.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+            Emaily 2018 | {''}
+            <a
+              class="orange-text text-lighten-3"
+              href="https://github.com/OceanRonquilloMorgan/emaily"
+            >
+              Source Code
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
